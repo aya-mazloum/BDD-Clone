67ee72c967dfe1c398bbdb661f8684c238cacfe8
aya-mazloum <mazloum.a.aya@gmail.com>
2024-03-03 20:14:11 +0200

    Add scroll to top feature

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,4 +1,5 @@
-window.onscroll = function() {scrollFunction()};
+const scrollTop = document.getElementById("scrollTop");
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
@@ -8,4 +9,14 @@ function scrollFunction() {
     document.getElementById("header-logo").setAttribute("src", "./assets/logo-light.png");
     document.getElementById("header").classList.remove("sticky-header");
   }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTop.style.display = "block";
+  } else {
+    scrollTop.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
